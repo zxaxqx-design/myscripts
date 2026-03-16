65618c63d346c1de63dc8c24e8a33e999c4148be
Andrey Sokolov <zxaxqx@gmail.com>
2026-03-16 23:38:31 +0400

    Remove all files

--- local_price.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path = '/srv/projects/100kwatt.ru/public_html/myscripts/epnew.xlsx';
-
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Length: '.filesize($path));
-
-readfile($path);
